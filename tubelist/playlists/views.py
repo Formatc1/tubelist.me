5821c50f9f8a40f64be9bf8e0396e555713e25cc
adam <edek-150@o2.pl>
2015-04-26 19:58:15 +0200

    playlisty trzymane w cookies i krotsze linki

--- tubelist/playlists/views.py
+++ tubelist/playlists/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.mail import send_mail
 from playlists.models import Playlist, Video
-from hashlib import md5
+from base64 import urlsafe_b64encode
+from struct import pack
 from datetime import datetime
 from apiclient.discovery import build
 # from apiclient.errors import HttpError
 
 
 def index(request):
+    if 'playlists' in request.COOKIES.keys():
+        playlists = request.COOKIES['playlists'].split(':')
+        user_playlists = Playlist.objects.filter(url__in=playlists)
+        return render(request, 'playlists/index.html', {'playlists': user_playlists})
     return render(request, 'playlists/index.html')
 
 
 def new(request):
-    url = md5(str(datetime.now())).hexdigest()
+    url = urlsafe_b64encode(pack(">Q", int(datetime.now().strftime('%y%m%d%H%M%S%f')))).replace('=', '')
     try:
         if request.POST['author'] != '':
             validate_email(request.POST['author'])
@@ -31,8 +37,16 @@ def new(request):
 
 def playlist(request, playlist_id):
     p = get_object_or_404(Playlist, url=playlist_id)
-    return render(request, 'playlists/playlist.html', {'playlist': p,
-        'playlist_id': playlist_id})
+    response = render(request, 'playlists/playlist.html', {'playlist': p})
+    if 'playlists' in request.COOKIES.keys():
+        playlists = request.COOKIES['playlists'].split(':')
+        if playlist_id not in playlists:
+            playlists.append(playlist_id)
+        playlists = ':'.join(playlists)
+    else:
+        playlists = playlist_id
+    response.set_cookie(key='playlists', value=playlists, max_age=31536000)
+    return response
 
 
 def search(request, playlist_id):
@@ -59,6 +73,29 @@ def search(request, playlist_id):
 def add(request, playlist_id, video_id, video_name):
     p = get_object_or_404(Playlist, url=playlist_id)
     p.video_set.add(Video(identifier=video_id, name=video_name,
-        order=len(p.video_set.all())+1))
+        order=p.sorted_video_set.latest('order').order + 1))
     return HttpResponseRedirect(reverse('playlists:playlist',
-            args=(p.url,)))
+        args=(p.url,)))
+
+
+def delete(request, playlist_id, video_id):
+    p = get_object_or_404(Playlist, url=playlist_id)
+    try:
+        p.video_set.filter(identifier=video_id)[0].delete()
+    except (ObjectDoesNotExist, IndexError):
+        pass
+    return HttpResponseRedirect(reverse('playlists:playlist',
+        args=(p.url,)))
+
+
+def recover(request):
+    if 'author' in request.POST.keys():
+        playlists = Playlist.objects.filter(author=request.POST['author'])
+        if not playlists:
+            return render(request, 'playlists/recover.html')
+        else:
+            result = '\r\n'.join(['%s - %s' % (x.name, x.url) for x in playlists])
+            send_mail('Playlisty', result, 'playlisty@tubelist.me', [request.POST['author']])
+            return render(request, 'playlists/recover.html', {'message': request.POST['author']})
+    else:
+        return render(request, 'playlists/recover-form.html')
