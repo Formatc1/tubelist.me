8fbdcae689e651b618da35bd9cf62b3bc50412dc
adam <edek-150@o2.pl>
2015-06-04 14:32:44 +0200

    unit tests

--- playlists/urls.py
+++ playlists/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns(
     url(r'^how-to/$', views.how_to, name='how_to'),
     url(r'^(?P<playlist_id>\w+)/$', views.playlist, name='playlist'),
     url(r'^(?P<playlist_id>\w+)/search/$', views.search, name='search'),
-    url(r'^(?P<playlist_id>\w+)/add/(?P<video_id>[a-zA-z0-9_-]+)/\
-(?P<video_name>.+)/$', views.add, name='add'),
+    url(r'^(?P<playlist_id>\w+)/add/(?P<video_id>[a-zA-z0-9_-]+)/$',
+        views.add, name='add'),
     url(r'^(?P<playlist_id>\w+)/delete/(?P<video_id>\d+)/$',
         views.delete, name='delete'),
 )
